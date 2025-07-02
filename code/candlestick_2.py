b1484bbd36ab9827f653059c9a1ab137aaed842e
Dusnoki Stefan <sdusnoki@constructor.university>
2025-07-02 18:26:00 +0200

    Developed function that converts a validated .txt file into .json format

--- code/candlestick_2.py
+++ code/candlestick_2.py
@@ -125,6 +125,46 @@ def sentiment_analysis(filePath: str) -> dict:
   return members_sentiment
 
 """
+Convert an exported .txt file to .json format.
+
+Args:
+  filePath (str): The path to the validated chat logs file.
+
+Returns:
+  str: The path to the resulting .json file.
+"""
+def convert_txt_to_json(filePath: str) -> str:
+  fileName = ntpath.basename(filePath).removesuffix(".txt")
+
+  date_pattern = re.compile(r'\b\d{1,2}/\d{1,2}/\d{2}\b')
+  time_pattern = re.compile(r'\d{2}:\d{2}')
+  name_pattern = re.compile(r'(?<=- )(.*?)(?=:)')
+  msg_pattern = re.compile(r'(?<=: )(.*)$')
+
+  with open(filePath, 'r') as f1, open(f"{fileName}.json", 'w', encoding = "utf-8") as f2:
+    f2.write('{\n\t"conversation": [\n')
+
+    comma_flag = True
+    for line in f1:
+      date = date_pattern.search(line).group()
+      time = time_pattern.search(line).group()
+      name = name_pattern.search(line).group()
+      msg = msg_pattern.search(line).group()
+
+      # Prevents commas at the start of the file / trailing commas
+      if comma_flag:
+        comma_flag = False
+      else:
+        f2.write(",\n")
+
+      json_str = orjson.dumps({"date":date, "message":msg, "time":time, "username":name}).decode("utf-8")
+      f2.write(f'\t\t{json_str}')
+    
+    f2.write('\n\t]\n}\n')
+
+  return os.path.abspath(f"{fileName}.json")
+
+"""
 Takes sentiment_analysis()'s resulting dictionary and creates a sentiment chart for each member.
 Resulting directory can be downloaded.
 
