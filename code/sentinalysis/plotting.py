796a130af56d43af14073a5ecbb64df277accfe1
Dusnoki Stefan <sdusnoki@constructor.university>
2025-07-22 20:15:18 +0200

    Made it so that the plotting function takes an extra output path argument

--- code/sentinalysis/plotting.py
+++ code/sentinalysis/plotting.py
@@ -8,13 +8,14 @@ Resulting directory can be downloaded.
 
 Args:
   sentiment_dictionary (dict): A dictionary of all chat members and their sentiment scores.
-
+  outputPath (str): The path of the directory where the validated .txt file will be saved.
+  
 Returns:
   None
 """
-def get_charts(sentiment_dictionary: dict) -> None:
-  os.makedirs(f"{os.getcwd()}/sentiment_chart_images/", exist_ok = True)
-  directory_path = os.path.abspath("sentiment_chart_images")
+def get_charts(sentiment_dictionary: dict, outputPath: str) -> None:
+  new_directory_path = os.path.join(outputPath, "sentiment_chart_images/")
+  os.makedirs(new_directory_path, exist_ok = True)
 
   for group_member in sentiment_dictionary:
     y = np.array([score for score, _ in sentiment_dictionary[group_member]])
@@ -26,5 +27,5 @@ def get_charts(sentiment_dictionary: dict) -> None:
     plt.title(f"{group_member}'s positivity score")
     plt.gcf().autofmt_xdate(rotation = 45)
 
-    plt.savefig(f"{directory_path}/{group_member}-sentiment.png", bbox_inches='tight')
+    plt.savefig(os.path.join(new_directory_path, f"{group_member}-sentiment.png"), bbox_inches='tight')
     plt.close()
